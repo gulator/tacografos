018e9e8a3602215bfd10286607e6236eaf04eca6
jdascanio <julian.dascanio@gmail.com>
2025-01-20 16:27:03 -0300

    R1

--- tacografos/settings.py
+++ tacografos/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = config('SECRET_KEY')
+# SECURITY WARNING: keep the secret key used in production secret!f
+#SECRET_KEY = config('SECRET_KEY') 
 SECRET_KEY = 'django-insecure-$d-158l654+ueobk_s1acak%$5x)w(hi+qqcqzhzz_0*67^a0%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
